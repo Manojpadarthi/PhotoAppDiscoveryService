2282b72e020a13907dcc2eb9acee531eb3d254a7
manoj <padarthi30@gmail.com>
2022-01-02 09:47:34 +0530

    new commit

--- src/main/java/com/example/photoapp/WebSecurity.java
+++ src/main/java/com/example/photoapp/WebSecurity.java
@@ -3,7 +3,6 @@ package com.example.photoapp;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter
 {
